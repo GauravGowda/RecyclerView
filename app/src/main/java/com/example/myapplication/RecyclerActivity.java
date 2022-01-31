73b78c75ab877c31a708a8284e423614bc9aaa7a
gauravgowda <gaurav.gowda55@gmail.com>
2022-01-31 12:52:51 +0530

    android get text,contacts and maps

--- app/src/main/java/com/example/myapplication/RecyclerActivity.java
+++ app/src/main/java/com/example/myapplication/RecyclerActivity.java
@@ -25,7 +25,7 @@ public class RecyclerActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.w(TAG,"starting");
-
+        //throw new NullPointerException();
     }
 
 protected void onResume() {
